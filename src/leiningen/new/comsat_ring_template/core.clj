9fa215452ad432d84c88e6c12d9c90548d5d9ba2
circlespainter <fabio@paralleluniverse.co>
2015-01-22 16:24:54 +0100

    :fire: Removed useless code

--- src/leiningen/new/comsat_ring_template/core.clj
+++ src/leiningen/new/comsat_ring_template/core.clj
@@ -1,25 +1,24 @@
-(ns  {{name}}.core
+(ns {{name}}.core
     (:use co.paralleluniverse.fiber.ring.jetty9)
-    (:require [co.paralleluniverse.pulsar.core :as pc]
+    (:require
+      [co.paralleluniverse.pulsar.core :refer [sfn defsfn suspendable!]]
       [ring.middleware.json :as midjson]
       [ring.middleware.resource :as midres]
       [net.cgrand.moustache :as moustache]
       [ring.util.response :as ringres])
     (:import (co.paralleluniverse.fibers Fiber)))
 
-(defn- as-sus! [h] (if (pc/suspendable? h) h (pc/suspendable! h)))
-
 (def ^:private app-routes (moustache/app
-                            [] (pc/sfn [_] (Fiber/sleep 100) (ringres/resource-response "index.html" {:root "public"}))
-                            ["widgets"] (pc/sfn [_] (Fiber/sleep 100) (ringres/response [{:name "Widget 1"} {:name "Widget 2"}]))))
+                            [] (sfn [_] (Fiber/sleep 100) (ringres/resource-response "index.html" {:root "public"}))
+                            ["widgets"] (sfn [_] (Fiber/sleep 100) (ringres/response [{:name "Widget 1"} {:name "Widget 2"}]))))
 
 (def app
   (-> app-routes
-      as-sus!
+      suspendable!
       (midres/wrap-resource "/public")
-      as-sus!
+      suspendable!
       (midjson/wrap-json-body)
-      as-sus!
+      suspendable!
       (midjson/wrap-json-response)))
 
 (defn run [] (run-jetty app {:port 8080}))
