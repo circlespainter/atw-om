5acc0403604c041dc5aa76d2b1ee65507e1727d8
circlespainter <fabio@paralleluniverse.co>
2015-10-28 15:52:19 +0200

    :arrow_up:

--- src/leiningen/new/comsat_ring_template/project.clj
+++ src/leiningen/new/comsat_ring_template/project.clj
@@ -2,27 +2,25 @@
   :description "TODO FIXME: write this!"
   :url "http://example.com/TODO FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [co.paralleluniverse/comsat-ring-jetty9 "0.4.0"]
-                 [ring/ring-json "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [co.paralleluniverse/comsat-ring-jetty9 "0.5.0"]
+                 [ring/ring-json "0.4.0"]
                  [net.cgrand/moustache "1.1.0" :exclusions [org.clojure/clojure ring/ring-core]]
-                 [org.clojure/clojurescript "0.0-3211"]
+                 [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [cljs-http "0.1.35"]
+                 [cljs-http "0.1.37"]
                  [org.omcljs/om "0.8.8"]
-                 [figwheel "0.3.3"]]
+                 [figwheel "0.4.1"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
+  :plugins [[lein-cljsbuild "1.1.0"]
             [lein-pdo "0.1.1"]
-            [lein-figwheel "0.3.3"]]
+            [lein-figwheel "0.4.1"]]
 
   :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "run"]}
 
-  :min-lein-version "2.5.0"
-
   :main {{name}}.core/run
 
-  :java-agents [[co.paralleluniverse/quasar-core "0.7.2"
+  :java-agents [[co.paralleluniverse/quasar-core "0.7.3"
                  ; :options "vdmc"
                  ]]
 
